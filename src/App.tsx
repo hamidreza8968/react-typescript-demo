c32d9279a3d7880df50f4a56f93b98ade4563ffc
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2024-01-18 17:56:31 +0100

    Add: "Button" component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import PersonList from "./components/PersonList/PersonList";
 import Status from "./components/Status/Status";
 import Heading from "./components/Heading/Heading";
 import Oscar from "./components/Oscar/Oscar";
+import Button from "./components/Button/Button";
 
 function App() {
     const personName = {
@@ -36,7 +37,7 @@ function App() {
             <Oscar>
                 <Heading>Oscar goes to Leonardo Dicaprio</Heading>
             </Oscar>
-
+            <Button handleClick={(e , id) => console.log("Button clicked!" , e , id)}/>
         </div>
     );
 }
