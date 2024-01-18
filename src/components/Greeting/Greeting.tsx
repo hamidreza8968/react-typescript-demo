afafbf697627893c7876c1ad2bf06d44cc5b343b
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2024-01-18 14:34:45 +0100

    Add: "Greeting" component

---
+++ src/components/Greeting/Greeting.tsx
@@ -0,0 +1,14 @@
+type GreetingProps = {
+    name: string,
+    messageNum: number,
+    isLoggedIn: boolean,
+}
+
+function Greeting(prop: GreetingProps) {
+    return (
+        <div>
+            <h2>Welcome {prop.isLoggedIn ? `${prop.name}! You have ${prop.messageNum} unread messages.` : "guest!"}</h2>
+        </div>
+    )
+}
+export default Greeting;
